5a9d251a388a76d753e516d665c9e637a9c48549
colyerdeng <colyerdeng@tencent.com>
2025-05-30 18:01:18 +0800

    feat: check template var to raw string

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,11 +5,12 @@ import pkgutil
 
 import repo_scaffold
 
+
 def test_imports():
     """Test import modules."""
-    prefix = "{}.".format(repo_scaffold.__name__) # noqa
+    prefix = "{}.".format(repo_scaffold.__name__)  # noqa
     iter_packages = pkgutil.walk_packages(
-        repo_scaffold.__path__,  # noqa
+        repo_scaffold.__path__,
         prefix,
     )
     for _, name, _ in iter_packages:
